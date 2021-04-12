64c45bc16eb3fd19f358c4d9934e036c1a59ef46
Alex <js.fullstack.development@gmail.com>
2021-04-12 11:26:05 +0300

    Правки

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
+const API_KEY = '20244739-961dea85be28be305e8bcd893';
+const BASE_URL = 'https://pixabay.com/api';
+axios.defaults.baseURL = BASE_URL;
+
 export const fetchImages = (searchQuery = '', currentPage = 1) => {
-  const API_KEY = '20244739-961dea85be28be305e8bcd893';
-  const BASE_URL = 'https://pixabay.com/api';
-
-  axios.defaults.baseURL = BASE_URL;
-
   const url = `?q=${searchQuery}&page=${currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
   return axios.get(url).then(res => res.data.hits);
